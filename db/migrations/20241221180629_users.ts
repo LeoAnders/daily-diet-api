7a2066f370f2a6b9557b4357792f2e47e4d0cf4d
LeoAnders <leonardoanders15@gmail.com>
2024-12-21 15:28:01 -0300

    Configurar migrations

--- db/migrations/20241221180629_users.ts
+++ db/migrations/20241221180629_users.ts
@@ -2,9 +2,14 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('users', (table) => {
+    table.uuid('id').primary()
+    table.string('user', 250).notNullable()
+    table.string('email', 320).unique().notNullable()
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable()
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('users')
 }
-
--- db/migrations/20241221180634_meals.ts
+++ db/migrations/20241221180634_meals.ts
@@ -2,9 +2,17 @@ import type { Knex } from "knex";
 
 
 export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable('meals', (table) => {
+    table.uuid('id').primary();
+    table.uuid('id').references('id').inTable('users').onDelete("CASCADE");
+    table.string('name', 255).notNullable();
+    table.text('description').nullable();
+    table.timestamp('datetime').notNullable();
+    table.boolean('is_on_diet').defaultTo(false);
+    table.timestamp('created_at').defaultTo(knex.fn.now()).notNullable();
+  })
 }
-
 
 export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable('meals')
 }
-
